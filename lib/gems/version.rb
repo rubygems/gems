8014ced3afb57173942c3c9d496c32fd637d9496
Erik Michaels-Ober <sferik@gmail.com>
2013-07-12 08:38:41 +0200

    Bump version to 0.8.2

--- lib/gems/version.rb
+++ lib/gems/version.rb
@@ -1,3 +1,17 @@
 module Gems
-  VERSION = "0.8.1" unless defined?(Gems::VERSION)
+  class Version
+    MAJOR = 0 unless defined? Gems::Version::MAJOR
+    MINOR = 8 unless defined? Gems::Version::MINOR
+    PATCH = 2 unless defined? Gems::Version::PATCH
+    PRE = nil unless defined? Gems::Version::PRE
+
+    class << self
+      # @return [String]
+      def to_s
+        [MAJOR, MINOR, PATCH, PRE].compact.join('.')
+      end
+    end
+  end
+
+  VERSION = Version.to_s
 end
