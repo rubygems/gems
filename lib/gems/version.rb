577b04cac8240b5f0fe05c799d732c3b0b6b61ec
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2018-02-26 12:32:51 +0900

    bump version to 1.1.0

--- lib/gems/version.rb
+++ lib/gems/version.rb
@@ -1,7 +1,7 @@
 module Gems
   class Version
     MAJOR = 1 unless defined? Gems::Version::MAJOR
-    MINOR = 0 unless defined? Gems::Version::MINOR
+    MINOR = 1 unless defined? Gems::Version::MINOR
     PATCH = 0 unless defined? Gems::Version::PATCH
     PRE = nil unless defined? Gems::Version::PRE
 
