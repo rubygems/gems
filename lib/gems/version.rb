30d472ac4d35202ca5b729b02ce0497ff3bed781
Erik Berlin <sferik@gmail.com>
2026-09-09 15:20:14 -0700

    Move the library version to Gems::VERSION

--- lib/gems/version.rb
+++ lib/gems/version.rb
@@ -1,23 +1,4 @@
 module Gems
-  # The version of the Gems gem
-  class Version
-    MAJOR = 2 unless defined? Gems::Version::MAJOR
-    MINOR = 0 unless defined? Gems::Version::MINOR
-    PATCH = 0 unless defined? Gems::Version::PATCH
-    PRE = nil unless defined? Gems::Version::PRE
-
-    class << self
-      # The version as a string
-      #
-      # @api public
-      # @return [String] the version string
-      # @example Get the version string
-      #   Gems::Version.to_s
-      def to_s
-        [MAJOR, MINOR, PATCH, PRE].compact.join(".")
-      end
-    end
-  end
-
-  VERSION = Version.to_s
+  # The current version of the Gems gem
+  VERSION = "2.0.0".freeze
 end
